f481c42e9d702d3f1e11179aa21bb3f72ef458dd
Chris <christoph.menzel@studium.fau.de>
2013-05-10 19:17:09 +0200

    Added a SharedAssemblyInfo

--- src/GraphalyzerPro/SharedAssemblyInfo.cs
+++ src/GraphalyzerPro/SharedAssemblyInfo.cs
@@ -1,4 +1,4 @@
-ï»¿/*
+/*
  * Copyright (c) 2006-2009 by Christoph Menzel, Daniel Birkmaier, 
  * Maximilian Madeja, Farruch Kouliev, Stefan Zoettlein
  *
@@ -21,13 +21,31 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GraphalyzerPro.CsvReceiver")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GraphalyzerPro")]
+[assembly: AssemblyCopyright("Copyright ©  2013. All rights reserved")]
+[assembly: AssemblyTrademark("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e5cd0c11-2a4f-407b-ab49-ccabc9d1eda8")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
