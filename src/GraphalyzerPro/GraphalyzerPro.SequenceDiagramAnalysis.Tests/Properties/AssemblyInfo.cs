83d7bb3aa0a9d12335084ddc1d33e82ab180dc77
Chris <christoph.menzel@studium.fau.de>
2013-06-01 15:14:37 +0200

    Implemented US#26 for displaying processes

--- src/GraphalyzerPro/GraphalyzerPro.SequenceDiagramAnalysis.Tests/Properties/AssemblyInfo.cs
+++ src/GraphalyzerPro/GraphalyzerPro.SequenceDiagramAnalysis.Tests/Properties/AssemblyInfo.cs
@@ -20,18 +20,41 @@
  */
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("GraphalyzerPro.SequenceDiagramAnalysis")]
+[assembly: AssemblyTitle("GraphalyzerPro.SequenceDiagramAnalysis.Tests")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GraphalyzerPro.SequenceDiagramAnalysis.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("cdb61747-ec65-4fe8-928a-0c42a57125bd")]
-[assembly: InternalsVisibleTo("GraphalyzerPro.SequenceDiagramAnalysis.Tests")]
+[assembly: Guid("ef7119ca-9a32-412b-a225-a65589664a21")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
