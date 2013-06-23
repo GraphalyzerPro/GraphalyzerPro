7958b287574652fe2109639bd3f9ce1b3abe3f6b
Chris <christoph.menzel@studium.fau.de>
2013-06-23 16:09:52 +0200

    Implemented autoscroll behavior for the sequence diagramm

--- src/GraphalyzerPro/GraphalyzerPro.SequenceDiagramAnalysis/SequenceDiagram.xaml.cs
+++ src/GraphalyzerPro/GraphalyzerPro.SequenceDiagramAnalysis/SequenceDiagram.xaml.cs
@@ -68,45 +68,88 @@ namespace GraphalyzerPro.SequenceDiagramAnalysis
         {
             base.OnMouseMove(e);
 
-            RefreshCrossairPosition();
+            if (ScrollViewer.IsMouseOver)
+            {
+                RefreshCrossairPosition();
+            }
         }
 
         private void ScrollViewerOnMouseEnter(object sender, MouseEventArgs e)
         {
-            CrosshairTopLine.Visibility =
-                CrosshairBottomLine.Visibility =
-                CrosshairRightLine.Visibility = CrosshairLeftLine.Visibility = Visibility.Visible;
+            EnableCrosshair();
         }
 
         private void ScrollViewerOnMouseLeave(object sender, MouseEventArgs e)
         {
-            CrosshairTopLine.Visibility =
-                CrosshairBottomLine.Visibility =
-                CrosshairRightLine.Visibility = CrosshairLeftLine.Visibility = Visibility.Collapsed;
+            DisableCrosshair();
         }
 
         private void ScrollViewerOnScrollChanged(object sender, ScrollChangedEventArgs e)
         {
-            RefreshCrossairPosition();
+            if (ScrollViewer.IsMouseOver)
+            {
+                RefreshCrossairPosition();
+            }
         }
 
         private void RefreshCrossairPosition()
         {
             var position = Mouse.GetPosition(MainGrid);
 
-            CrosshairTopLine.X1 = CrosshairTopLine.X2 = position.X;
-            CrosshairTopLine.Y1 = position.Y - 15;
+            if (position.Y > MainGrid.ActualHeight - 2 || position.X > MainGrid.ActualWidth - 2)
+            {
+                DisableCrosshair();
+            }
+            else
+            {
+                EnableCrosshair();
+                
+                if (position.Y < MainGrid.ActualHeight - 15)
+                {
+                    CrosshairBottomLine.Visibility = Visibility.Visible;
 
-            CrosshairBottomLine.X1 = CrosshairBottomLine.X2 = position.X;
-            CrosshairBottomLine.Y1 = position.Y + 15;
-            CrosshairBottomLine.Y2 = MainGrid.ActualHeight;
+                    CrosshairBottomLine.X1 = CrosshairBottomLine.X2 = position.X;
+                    CrosshairBottomLine.Y1 = position.Y + 15;
+                    CrosshairBottomLine.Y2 = MainGrid.ActualHeight - 15;
+                }
+                else
+                {
+                    CrosshairBottomLine.Visibility = Visibility.Collapsed;
+                }
 
-            CrosshairRightLine.Y1 = CrosshairRightLine.Y2 = position.Y;
-            CrosshairRightLine.X1 = position.X - 15;
+                if (position.X < MainGrid.ActualWidth - 15)
+                {
+                    CrosshairRightLine.Visibility = Visibility.Visible;
 
-            CrosshairLeftLine.Y1 = CrosshairLeftLine.Y2 = position.Y;
-            CrosshairLeftLine.X1 = position.X + 15;
-            CrosshairLeftLine.X2 = MainGrid.ActualWidth;
+                    CrosshairRightLine.Y1 = CrosshairRightLine.Y2 = position.Y;
+                    CrosshairRightLine.X1 = position.X + 15;
+                    CrosshairRightLine.X2 = MainGrid.ActualWidth - 15;
+                }
+                else
+                {
+                    CrosshairRightLine.Visibility = Visibility.Collapsed;
+                }
+
+                CrosshairTopLine.X1 = CrosshairTopLine.X2 = position.X;
+                CrosshairTopLine.Y1 = position.Y - 15;
+
+                CrosshairLeftLine.Y1 = CrosshairLeftLine.Y2 = position.Y;
+                CrosshairLeftLine.X1 = position.X - 15;
+            }
+        }
+
+        private void EnableCrosshair()
+        {
+            CrosshairTopLine.Visibility =
+                CrosshairBottomLine.Visibility =
+                CrosshairLeftLine.Visibility = CrosshairRightLine.Visibility = Visibility.Visible;
+        }
+
+        private void DisableCrosshair()
+        {
+            CrosshairTopLine.Visibility =
+                CrosshairBottomLine.Visibility =
+                CrosshairLeftLine.Visibility = CrosshairRightLine.Visibility = Visibility.Collapsed;
         }
     }
 }
